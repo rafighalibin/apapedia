12d1840e090936b44c01b6550a54d9da436bc823
rafighalibin <rafighalibin@ui.ac.id>
2023-11-10 16:58:42 +0700

    completed GET allCategory

---
+++ catalogue/src/main/java/com/apapedia/catalogue/model/Category.java
@@ -0,0 +1,26 @@
+package com.apapedia.catalogue.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    public UUID id = UUID.randomUUID();
+
+    @NotNull
+    @Size(max = 240)
+    private String name;
+}
