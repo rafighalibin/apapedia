c55d0d420b4096f9a2ae1c413f078ad157e5f18c
rafighalibin <rafighalibin@ui.ac.id>
2023-11-10 17:56:33 +0700

    feat: view profile (not finished)

---
+++ frontend/src/main/java/com/apapedia/frontend/controller/ProfileController.java
@@ -0,0 +1,21 @@
+package com.apapedia.frontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile/")
+    public String profilePage(Model model) {
+        model.addAttribute("id", "1234567890");
+        model.addAttribute("name", "John Doe");
+        model.addAttribute("username", "johndoe");
+        model.addAttribute("email", "john@mail");
+        model.addAttribute("address", "Jl. Jalan No. 1");
+        return "profile-view";
+    }
+
+}
