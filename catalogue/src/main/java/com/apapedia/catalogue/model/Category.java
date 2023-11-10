c9eade904c5972d74e4bebb8756688c94b6978cc
andreshapradana <andresha.pradana@gmail.com>
2023-11-10 16:30:34 +0700

    complete POST CATALOG

---
+++ catalogue/src/main/java/com/apapedia/catalogue/model/Category.java
@@ -0,0 +1,24 @@
+package com.apapedia.catalogue.model;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    private UUID id;
+    private String name;
+
+    // getters and setters
+}
