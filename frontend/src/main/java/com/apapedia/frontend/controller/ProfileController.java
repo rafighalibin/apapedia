a39ef68103faf0cfbb6ac1e7d01fed7f3ddeed55
rafighalibin <rafighalibin@ui.ac.id>
2023-11-14 18:45:38 +0700

    fix: duplicated catalogue files

--- frontend/src/main/java/com/apapedia/frontend/controller/ProfileController.java
+++ frontend/src/main/java/com/apapedia/frontend/controller/ProfileController.java
@@ -1,25 +1,16 @@
 package com.apapedia.frontend.controller;
 
-<<<<<<< frontend/src/main/java/com/apapedia/frontend/controller/ProfileController.java
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-=======
 import java.io.IOException;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import com.apapedia.frontend.DTO.response.ReadUserResponseDTO;
 import com.apapedia.frontend.service.UserService;
->>>>>>> frontend/src/main/java/com/apapedia/frontend/controller/ProfileController.java
 import org.springframework.ui.Model;
 
 @Controller
 public class ProfileController {
-
 
     @Autowired
     UserService userService;
