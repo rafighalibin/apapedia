a0dfe4a24a90320516d6978c44327254c05e1c97
Rafi Ghalibin Abrar <rafi.ghalibin@ui.ac.id>
2023-11-22 15:49:11 +0700

    Abin/order/init service

---
+++ order/src/main/java/com/apapedia/order/dto/request/CreateCartItemRequestDTO.java
@@ -0,0 +1,21 @@
+package com.apapedia.order.dto.request;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CreateCartItemRequestDTO {
+
+    private UUID productId;
+
+    private UUID cartId;
+
+    @NotNull
+    private int quantity;
+}
---
+++ order/src/main/java/com/apapedia/order/model/CartItem.java
@@ -0,0 +1,32 @@
+package com.apapedia.order.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cart_item")
+public class CartItem {
+
+    @Id
+    private UUID id = UUID.randomUUID();
+
+    @Column(name = "id_product", nullable = false)
+    private UUID productId;
+
+    @Column(name = "id_cart", nullable = false)
+    private UUID cartId;
+
+    @NotNull
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+}
