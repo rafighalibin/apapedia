b1c074322fb8b5b05126202a06d6e90045fe1f24
Jessica Lambok <jessica.lambok@ui.ac.id>
2023-12-07 15:24:52 +0700

    Pindah seller order history dari modul order ke modul frontend

--- frontend/src/main/java/com/apapedia/frontend/service/OrderService.java
+++ frontend/src/main/java/com/apapedia/frontend/service/OrderService.java
@@ -1,12 +1,17 @@
 package com.apapedia.frontend.service;
 
+import com.apapedia.frontend.core.Order;
 import org.springframework.stereotype.Service;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public interface OrderService {
-    HashMap<Integer, Integer> getGraph(HttpServletRequest request);
+    HashMap<Integer, Integer> getGraph();
+
+    List<Order> findBySellerId(UUID sellerId);
 }
