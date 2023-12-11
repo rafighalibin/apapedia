61c2010ba78686a820b462fbee11c0134e39d742
Muhammad Arief Braja Putra <muhammad.arief17@ui.ac.id>
2023-12-11 15:26:21 +0700

    finish viewall catalogue

--- catalogue/src/main/java/com/apapedia/catalogue/controller/CategoryController.java
+++ catalogue/src/main/java/com/apapedia/catalogue/controller/CategoryController.java
@@ -1,7 +1,6 @@
 package com.apapedia.catalogue.controller;
 
 import java.util.List;
-import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
